c1206f2e495256610981c2c60e9f5ab57b2a9066
beautiful <umidjonminecrafter@gmail.com>
2026-06-11 09:15:09 +0500

    Add Eskiz SMS verification logic using Django cache

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,16 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 # Asosiy Django ilovasi ishga tushadi
 application = get_wsgi_application()
 
-# Mavlumotlar bazasi bilan ishlaydigan kod aynan shu yerda (pastda) bo'lishi kerak:
-from django.contrib.auth import get_user_model
 
-try:
-    User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'parol12345')
-        print("Superuser muvaffaqiyatli yaratildi! ✅")
-except Exception as e:
-    print(f"Superuser yaratishda xatolik: {e}")
 
 
 try:

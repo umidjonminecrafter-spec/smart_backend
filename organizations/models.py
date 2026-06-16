9927c4811c19b41ad7486ff8c8e1d370ec7a89d7
beautiful <umidjonminecrafter@gmail.com>
2026-06-16 14:28:35 +0500

    zayballl

--- organizations/models.py
+++ organizations/models.py
@@ -18,6 +18,8 @@ class Organization(BaseModel):
     available_roles = models.JSONField(default=list, blank=True)
     address = models.TextField(null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
+    latitude = models.DecimalField(max_length=50, max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_length=50, max_digits=9, decimal_places=6, null=True, blank=True)
 
     def __str__(self):
         return self.name

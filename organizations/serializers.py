4ba50894654758a38d64b7a8e8606deb76063863
beautiful <umidjonminecrafter@gmail.com>
2026-06-16 14:50:16 +0500

    zayballl

--- organizations/serializers.py
+++ organizations/serializers.py
@@ -4,7 +4,7 @@ from finance.models import FinanceSetting, StaffSalaryPercent
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
-        fields = "__aLL__"
+        fields = '__aLL__'
         read_only_fields = ('id', 'created_at', 'updated_at')
 
 class BranchSerializer(serializers.ModelSerializer):

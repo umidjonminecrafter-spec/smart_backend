bc0d3c742e623ca7520bf721b415f326c374a9c0
beautiful <umidjonminecrafter@gmail.com>
2026-06-16 14:23:03 +0500

    zayballl

--- organizations/models.py
+++ organizations/models.py
@@ -17,6 +17,7 @@ class Organization(BaseModel):
     role_permissions = models.JSONField(default=dict, blank=True)
     available_roles = models.JSONField(default=list, blank=True)
     address = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.name

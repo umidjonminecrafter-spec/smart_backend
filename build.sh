35f245080bdb0d2ddc2a0f371c9f6a73063b8071
Sizning Ismingiz <umidjonminecrafter@gmail.com>
2026-06-20 14:06:09 +0500

    zayballl

--- build.sh
+++ build.sh
@@ -5,4 +5,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate academics --fake
+python manage.py migrate accounts --fake
+python manage.py migrate analytics --fake
+python manage.py migrate audit --fake
+python manage.py migrate billing --fake
+python manage.py migrate communication --fake
+python manage.py migrate config --fake
+python manage.py migrate crm --fake
+python manage.py migrate organizations --fake
+python manage.py migrate finance --fake
+python manage.py migrate tasks --fake
 python manage.py migrate
